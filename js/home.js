18957cc95e1ff7caa35f160e2d3c593ee3ba3796
Sumit Kumar <sumit.k@tallysolutions.com>
2019-07-23 17:01:48 +0530

    seeing

--- js/home.js
+++ js/home.js
@@ -1,6 +1,8 @@
-var itemImage=document.getElementsByClassName("item_image");
-var arrayImages=["band1.jpg","band2.jpg","cap1.jpg","cap2.jpg","chain1.jpg","chain2.jpg","shade1.jpg","shade2.jpg","vest1.jpg","vest2.jpg"];
-for(var i=0;i<itemImage.length;i++)
-{
-    itemImage[i].attributes(src,arrayImages[i]);
-}
+document.onload(function(){
+    var itemImage=document.getElementsByClassName("card_container");
+    var arrayImages=["band1.jpg","band2.jpg","cap1.jpg","cap2.jpg","chain1.jpg","chain2.jpg","shade1.jpg","shade2.jpg","vest1.jpg","vest2.jpg"];
+    for(var i=0;i<arrayImages.length;i++)
+    {
+        itemImage[i].innerHTML="<img class=\"item_image\" src=\'"+arrayImages[i]+"' width=\"15%\"><h4 class=\"item_name\"></h4><h4 class=\"item_price\"></h4><button class=\"buy_button\">BUY</button>";
+    }
+});
