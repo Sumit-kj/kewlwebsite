60418072e84bcfd5d4416578e1689c9da3e5eb64
Ankit <Ankit>
2019-07-29 16:50:35 +0530

    commitment

--- js/home.js
+++ js/home.js
@@ -1,4 +1,41 @@
-document.onload(function(){
+document.addEventListener("DOMContentLoaded",function(){
+    var cn=document.getElementsByClassName("ADD");
+    console.log(cn);
+    var myfunc=function() {
+        var attr=this.parentElement.childNodes[9];
+        console.log(attr);
+        var val2 = parseInt(attr.innerHTML);
+        val2++;
+        attr.innerHTML=val2;
+        console.log(attr);
+
+    }
+
+   
+        var cn2=document.getElementsByClassName("SUB");
+        console.log(cn2);
+        var subfunc=function() {
+            var attr2=this.parentElement.childNodes[9];
+            console.log(attr2);
+            var val3= parseInt(attr2.innerHTML);
+            if(val3==0)
+            {
+                this.disabled=true;
+            }
+            else{
+            val3--;
+            attr2.innerHTML=val3;
+            console.log(attr2);
+            }
+    
+        }
+    for (var i = 0; i < cn.length; i++) {
+        cn[i].addEventListener("click", myfunc, false);
+    }
+    for(var i=0;i<cn2.length;i++)
+    {
+        cn2[i].addEventListener("click",subfunc,false);
+    }
     var itemImage=document.getElementsByClassName("card_container");
     var arrayImages=["band1.jpg","band2.jpg","cap1.jpg","cap2.jpg","chain1.jpg","chain2.jpg","shade1.jpg","shade2.jpg","vest1.jpg","vest2.jpg"];
     for(var i=0;i<arrayImages.length;i++)
@@ -6,3 +43,5 @@ document.onload(function(){
         itemImage[i].innerHTML="<img class=\"item_image\" src=\'"+arrayImages[i]+"' width=\"15%\"><h4 class=\"item_name\"></h4><h4 class=\"item_price\"></h4><button class=\"buy_button\">BUY</button>";
     }
 });
+
+
--- php/signup.php
+++ php/signup.php
@@ -1,47 +1,47 @@
 <?php
-echo "page loaded";
-// define('DB_SERVER', '10.20.27.76:5500');
-// define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', '');
-// define('DB_DATABASE', 'kewlworld');
-// $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+// echo "page loaded";
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'kewlworld');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
-//    if($_SERVER["REQUEST_METHOD"] == "POST") {
-//       // username and password sent from form 
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
 
-//       $myusername = mysqli_real_escape_string($db,$_POST['Username']);
-//       $myname=mysqli_real_escape_string($db,$_POST['name']);
-//       $mypassword = mysqli_real_escape_string($db,$_POST['Password']);
+      $myusername = mysqli_real_escape_string($db,$_POST['Username']);
+      $myname=mysqli_real_escape_string($db,$_POST['name']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['Password']);
 
-//       echo "$myusername $myname $mypassword";
+      echo "$myusername $myname $mypassword";
 
-//       $sql = "SELECT * FROM customer WHERE EMAIL = '$myusername' AND PASS = '$mypassword'";
-//       $result = mysqli_query($db,$sql);
-//       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-//       $active = $row['active'];
+      $sql = "SELECT * FROM customer WHERE EMAIL = '$myusername' AND PASS = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
 
-//       $myid = $row['email'];
+      $myid = $row['email'];
       
-//       $count = mysqli_num_rows($result);
+      $count = mysqli_num_rows($result);
 
-//       // If result matched $myusername and $mypassword, table row must be 1 row
+      // If result matched $myusername and $mypassword, table row must be 1 row
         
       
-//       if($count == 1) {
+      if($count == 1) {
         
-//          header('Location: ../index.html');
-//          echo "<script>alert('User already exists');
-//          window.location.href='../form.html';</script>";
-//          exit;
-//       }else {
-//           session_start();
-//           $_SESSION['customer_id']=$myusername;
-//           $sql2="INSERT INTO `customer`(`EMAIL`, `NAME`, `PASS`) VALUES ($myusername,$myname,$mypassword)";
-//           echo "<script>alert('Successfully Registered ');
-//           window.location.href='../index.html';</script>";
+         header('Location: ../index.html');
+         echo "<script>alert('User already exists');
+         window.location.href='../form.html';</script>";
+         exit;
+      }else {
+          session_start();
+          $_SESSION['customer_id']=$myusername;
+          $sql2="INSERT INTO `customer`(`EMAIL`, `NAME`, `PASS`) VALUES ($myusername,$myname,$mypassword)";
+          echo "<script>alert('Successfully Registered ');
+          window.location.href='../index.html';</script>";
 
-//       }
-//     }
+      }
+    }
    
 
 ?>
